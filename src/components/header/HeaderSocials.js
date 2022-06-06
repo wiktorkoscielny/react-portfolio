347287a1168c5d0ee792b15906614d89a56da57a
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-06 19:30:48 +0200

    Created components for links and socials bar

---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://github.com/wiktorkoscielny' target="_blank"></a>
+        <a href='https://www.linkedin.com/in/wiktor-kościelny-262649232/' target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
