e5dcead4c8eca3628f3d69ea4e77ccc002a1b73b
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-06 20:18:39 +0200

    Add react icons

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+// react icons
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://github.com/wiktorkoscielny' target="_blank"></a>
-        <a href='https://www.linkedin.com/in/wiktor-kościelny-262649232/' target="_blank"></a>
+        <a href='https://github.com/wiktorkoscielny' target="_blank"><FaGithub /></a>
+        <a href='https://www.linkedin.com/in/wiktor-kościelny-262649232/' target="_blank"><BsLinkedin /></a>
     </div>
   )
 }
