047a6eeb1bd2c7e3dba63dd1b81aa867aa5f6b74
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-06 15:41:07 +0200

    Starting from the sratch, added components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+//components
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
