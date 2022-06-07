a1a123213150ca0db3d22eed914987ac998b4af6
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-07 16:14:28 +0200

    Created about JSX content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,58 @@
 import React from 'react'
 import './about.css'
 
+//images
+import ME from '../../assets/me-1.png'
+
+//icons
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Me Image'></img>
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>No commercial experience</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>No official clients</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>Every project at my repository</small>
+            </article>
+          </div>
+
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
