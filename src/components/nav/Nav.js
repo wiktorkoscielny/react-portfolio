5750c364b5eb44a81cde74964d022139dfb37b5a
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-07 09:46:43 +0200

    Created Nav bar with links to page sections

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
 
+//icons
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className='active'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><BiBook /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
