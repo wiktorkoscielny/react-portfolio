69540377d980479871c5e8812f2318709c54eada
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-07 20:55:10 +0200

    Add small amendments to Services component

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
         Services
       </h2>
 
-      <div className='container services__container'>
+      <div className='container service__container'>
         <article className='service'>
           <div className='service__head'>
             <h3>UI/UX Design</h3>
