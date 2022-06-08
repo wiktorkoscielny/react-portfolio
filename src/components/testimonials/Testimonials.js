fa67d654f64831f0e5c87021f35f7abe56b610f7
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-08 16:15:11 +0200

    Created Testimonials Component, added JSX and map method to Testimonials.js, I have also created carousel using SwiperJS  and created styles with responsive design at testimonials.css

---
+++ src/components/testimonials/Testimonials.js
@@ -0,0 +1,81 @@
+import React from "react";
+import './testimonials.css'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// avatars
+import AVTR1 from '../../assets/avatar-test.png'
+
+// generate Testimonials from array with map method
+const data = [
+    {
+        avatar: AVTR1,
+        alt: 'Avatar picture',
+        name: 'Tarnished',
+        review: 'The Tarnished are individuals who lost the grace of the Erdtre and were consequently banished from the Lands Between by Queen Marika the Eternal. Following the breaking of the Elden Ring, the Tarnished were beckoned by lost grace.'
+    },
+    {
+        avatar: AVTR1,
+        alt: 'Avatar picture',
+        name: 'Tarnished',
+        review: 'The Tarnished are individuals who lost the grace of the Erdtre and were consequently banished from the Lands Between by Queen Marika the Eternal. Following the breaking of the Elden Ring, the Tarnished were beckoned by lost grace.'
+    },
+    {
+        avatar: AVTR1,
+        alt: 'Avatar picture',
+        name: 'Tarnished',
+        review: 'The Tarnished are individuals who lost the grace of the Erdtre and were consequently banished from the Lands Between by Queen Marika the Eternal. Following the breaking of the Elden Ring, the Tarnished were beckoned by lost grace.'
+    },
+    {
+        avatar: AVTR1,
+        alt: 'Avatar picture',
+        name: 'Tarnished',
+        review: 'The Tarnished are individuals who lost the grace of the Erdtre and were consequently banished from the Lands Between by Queen Marika the Eternal. Following the breaking of the Elden Ring, the Tarnished were beckoned by lost grace.'
+    },
+]
+
+const Testimonials = () => {
+  return (
+      <section className="Testimonials">
+          
+        <h5>Rewiev from clients</h5>
+        <h2>Testimonials</h2>
+
+        <Swiper className="container testimonials__container"
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+        >
+
+            {
+                data.map(({avatar, alt, name, review}, index) => {
+                    return (
+                        <SwiperSlide key={index} className="testimonial">
+                            <div className="client__avatar">
+                                <img src={avatar} alt={alt}/>
+                            </div>
+                            <h5 className="client__name">{name}</h5>
+                            <small className="client__review">{review}</small>
+                        </SwiperSlide>
+                    )
+                })
+            }
+
+            
+            
+        </Swiper>
+
+      </section>
+  )
+}
+
+export default Testimonials
