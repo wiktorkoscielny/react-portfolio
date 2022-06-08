68e441c4d877d9aaff2d6a4e33698864ab97960a
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-08 18:19:32 +0200

    Add JSX elements to Contact component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,53 @@
 import React from 'react'
 import './contact.css'
 
+// icons
+import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {AiOutlineWhatsApp} from 'react-icons/ai'
+
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+
+        {/* LEFT SIDE */}
+        <div className='contact__options'>
+
+          <article className='contact__option'>
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>loremipsum@gmail.com</h5>
+            <a href='mailto:loremipsum@gmail.com' target='_blank'>Send Message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>loremipsum</h5>
+            <a href='https://m.me/lorem.ipsum' target='_blank'>Send Message</a>
+          </article>
+          <article className='contact__option'>
+            <AiOutlineWhatsApp />
+            <h4>WhatsApp</h4>
+            <h5>+1234567</h5>
+            <a href='https://api.whatsapp.com/send?phone=+4812345678'>Send Message</a>
+          </article>
+
+        </div>
+
+        {/* RIGHT SIDE */}
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input  type='email' name='email' placeholder='Your email' required/>
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+
+      </div>
+    </section>
   )
 }
 
